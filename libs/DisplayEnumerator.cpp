facc605acad54dc2eafd956c20dea88a509b1ddd
ide0224 <kozmaatilar@protonmail.com>
2025-09-26 07:11:58 +0300

    clean up comment

--- libs/DisplayEnumerator.cpp
+++ libs/DisplayEnumerator.cpp
@@ -163,11 +163,11 @@ void DisplayEnumerator::FillInfo(std::vector<QRect> &geometry, std::vector<QSize
         vinfo.CardName = videoadaptername[i];
 
         memcpy(info.EDID, edids[i].data(), info.EDIDsize);
-        //qDebug() << "EDID: " <<  edids[i].toHex();
-//        qDebug() << "EDID size: " <<  edids[i].size();
-//        qDebug() << "Position: " << info.left << info.top << "-" << info.width << info.height;
-//        qDebug() << "OS id:" << info.OSId;
-//        qDebug() << "---------------------------";
+        // Debug: EDID information for display enumeration
+        // qDebug() << "EDID: " << edids[i].toHex();
+        // qDebug() << "EDID size: " << edids[i].size();
+        // qDebug() << "Position: " << info.left << info.top << "-" << info.width << info.height;
+        // qDebug() << "OS id:" << info.OSId;
         displays_.push_back(info);
         videoadapters_.push_back(vinfo);
     }
@@ -323,181 +323,6 @@ void DisplayEnumerator::FillInfo(std::vector<QRect> &geometry, std::vector<QSize
             unfind_counter++;
         }
     }
-
-
-
-
-
-//        // check is it serial number already used by winapi return list of displays
-//        uint match(UINT_MAX);
-//        for (auto it = all_lg_displays.begin(); it != all_lg_displays.end(); ++it){
-//            if (displays_[it->first].serial() == newSerial){
-//                match = it->first;
-//                break;
-//            }
-//        }
-
-//        if (match != UINT_MAX){
-//            all_lg_displays.erase(match);
-//            continue;
-//        }
-//    }
-
-
-
-//    for (auto it : lg_uart_displays){
-//        auto edid = it.second.edid;
-//        QString newSerial("");
-//        for (uint e = 2; e < edid.size(); e++){
-//            if (edid[e - 2] == char(0x00) && edid[e - 1] == char(0x00) && edid[e] == char(0xFF)){
-//                for (uint c = 0; c < 13; c++){
-//                    char nc = edid[e + c + 2];
-//                    if ((nc >= 48 && nc <= 57) || (nc >= 65 && nc <= 90))
-//                        newSerial += nc;
-//                }
-//                break;
-//            }
-//        }
-//        if (newSerial.isEmpty())
-//            continue;
-
-//        // check is it serial number already used by winapi return list of displays
-//        uint match(UINT_MAX);
-//        for (auto it = all_lg_displays.begin(); it != all_lg_displays.end(); ++it){
-//            if (displays_[it->first].serial() == newSerial){
-//                match = it->first;
-//                break;
-//            }
-//        }
-
-//        if (match != UINT_MAX){
-//            all_lg_displays.erase(match);
-//            continue;
-//        }
-//    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    // FIXME try another connection to LG using UART port
-//    std::vector <uint> lgDisplays;
-//    std::vector <uint> lgKnownDisplays;
-//    for (uint i = 0; i < displays_.size(); i++){
-//        auto EDIDdata = displays_[i].EDID;
-//        qDebug() << "find displat EDID" << i << *displays_[i].EDID;
-//        if (EDIDdata[0x08] == 0x1E && EDIDdata[0x09] == 0x6D){
-//            bool find0xFF = false;
-
-//            QByteArray out = QByteArray::fromHex(QByteArray::fromRawData((char*)(EDIDdata), std::strlen((char*)(EDIDdata))));
-//            qDebug() << out;
-//            qDebug() << std::strlen((char*)(EDIDdata));
-//            qDebug() << QByteArray::fromRawData((char*)(EDIDdata), std::strlen((char*)(EDIDdata)));
-//            qDebug() << EDIDdata;
-//            qDebug() << "disaplys" << i << displays_[i].name() << displays_[i].EDID << displays_[i].EDIDsize << displays_[i].serial();
-//            QString rr = QString::fromLocal8Bit((char*)EDIDdata);
-//            for (uint e = 2; e < out.size(); e++){
-//                QString newSerial("");
-//                qDebug() << e << out[e - 2] << out[e - 1] << out[e];
-//                qDebug() << e << EDIDdata[e - 2] << EDIDdata[e - 1] << EDIDdata[e];
-//                qDebug() << e << rr[e - 2] << rr[e - 1] << rr[e];
-//                qDebug() << e << out[e - 2] << out[e - 1] << out[e];
-//                if (out[e - 2] == char(0x00) && out[e - 1] == char(0x00) && out[e] == char(0xFF)){
-//                    find0xFF = true;
-//                    for (uint c = 0; c < 13; c++){
-//                        char nc = out[e + c + 2];
-//                        if ((nc >= 48 && nc <= 57) || (nc >= 65 && nc <= 90))
-//                            newSerial += nc;
-//                    }
-//                    qDebug() << "0xFF found finally";
-//                    displays_[i].setSerial(newSerial);
-//                    break;
-//                }
-//            }
-
-//            qDebug() << "is found lgdisplays" << i << find0xFF;
-//            if (not find0xFF)
-//                lgDisplays.push_back(i);
-//        }
-//    }
-
-//    qDebug() << "size of lg uart serials:" << lgDisplays.size();
-//    if (lgDisplays.size() > 0){
-//        auto lgUartConnections = LgMedicalUartConnectionInfo::enumerateUartConnections();
-//        qDebug() << "lg uart connections size" << lgUartConnections.size();
-//        auto key = lgUartConnections.begin();
-//        for (auto k = lgUartConnections.begin(); k != lgUartConnections.end(); ++k)
-//            qDebug() << "lg uart  con" << k->first << k->second.edid << QString::fromLocal8Bit(k->second.edid);
-
-//        for (uint i = 0; i < lgDisplays.size() && key != lgUartConnections.end(); ++key){
-//            auto edid = key->second.edid;
-//            qDebug() << i << lgDisplays[i] << edid;
-//            QString newSerial("");
-//            for (uint e = 2; e < edid.size(); e++){
-//                if (edid[e - 2] == char(0x00) && edid[e - 1] == char(0x00) && edid[e] == char(0xFF)){
-//                    for (uint c = 0; c < 13; c++){
-//                        char nc = edid[e + c + 2];
-//                        if ((nc >= 48 && nc <= 57) || (nc >= 65 && nc <= 90))
-//                            newSerial += nc;
-//                    }
-//                    break;
-//                }
-//            }
-//            qDebug() << "new serial" << newSerial << bool(lgDisplays[i]) << lgDisplays.size();
-
-////            // check is it serial number is already used by another LG displays
-////            bool foundSameSerial = false;
-////            for (auto disp = displays_.begin(); disp != displays_.end(); ++disp){
-////                qDebug() << "try to found same serial number " << disp->EDID << newSerial << disp->serial();
-////                if (disp.operator*().serial() == newSerial && !newSerial.isEmpty()){
-////                    foundSameSerial = true;
-////                    break;
-////                }
-////            }
-
-////            if (foundSameSerial)
-////                continue;
-
-//            if (!newSerial.isEmpty()){
-//                qDebug() << "new serial is not empty";
-//                displays_[lgDisplays[i]].setSerial(newSerial);
-//                displays_[lgDisplays[i]].setDetectSerial(newSerial);
-//                lgKnownDisplays.push_back(lgDisplays[i]);
-//                i++;
-//            }
-//        }
-//    }
-
-//    std::vector <uint> lgUnknownDisplays;
-//    qDebug() << "lg unknown displays count" << lgUnknownDisplays.size();
-//    for (auto allLG = lgDisplays.begin(); allLG != lgDisplays.end(); ++allLG){
-//        bool isFound = false;
-//        for (auto knownLG = lgKnownDisplays.begin(); knownLG != lgKnownDisplays.end(); ++knownLG)
-//            if (knownLG.operator*() == allLG.operator*())
-//                isFound = true;
-//        if (not isFound)
-//            lgUnknownDisplays.push_back(allLG.operator*());
-//    }
-
-//    uint lgNumber = 1;
-//    for (auto disp = lgUnknownDisplays.begin(); disp != lgUnknownDisplays.end(); ++disp){
-//        displays_[disp.operator*()].setSerial      (QString("DISPLAY") + QString::number(lgNumber));
-//        displays_[disp.operator*()].setDetectSerial(QString("DISPLAY") + QString::number(lgNumber));
-//        lgNumber++;
-//    }
-
-    qDebug() << "out of function";
 }
 
 void DisplayEnumerator::FillInfo(const DisplayInfo &info)
@@ -537,9 +362,9 @@ void DisplayEnumerator::joinWideDisplays()
     for (uint i = 0; int(i) < n; i++)
         if (isWideLeftPart(displays_[i])){
             auto &leftPart = displays_[i];
-            const bool isHorisontal = leftPart.width() < leftPart.height();
+            const bool isHorizontal = leftPart.width() < leftPart.height();
 
-            qDebug() << "left part" << i << "geometry:" << leftPart.left() << leftPart.top() << leftPart.width() << leftPart.height() << isHorisontal;
+            qDebug() << "left part" << i << "geometry:" << leftPart.left() << leftPart.top() << leftPart.width() << leftPart.height() << isHorizontal;
 
             for (uint j = 0; int(j) < n; j++)
                 if (i != j && !removed[j] && isWideRightPart(displays_[j]))
@@ -552,18 +377,18 @@ void DisplayEnumerator::joinWideDisplays()
 
                     const int gap = 20;
 
-                    if (isHorisontal && (abs(leftPart.top() - rightPart.top()) <= gap &&
+                    if (isHorizontal && (abs(leftPart.top() - rightPart.top()) <= gap &&
                                          abs(leftPart.left() + leftPart.width() - rightPart.left()) <= gap) )
                     {
                         leftPart.setWidth(leftPart.width() + rightPart.width());
                         leftPart.setResolutionWidth(leftPart.resolutionWidth() + rightPart.resolutionWidth());
                         removed[j] = true;
-                        qDebug() << "join displays horisontally" << i << j;
+                        qDebug() << "join displays horizontally" << i << j;
                         break;
                     }
 
                     // clock-wise rotation
-                    if (!isHorisontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
+                    if (!isHorizontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
                                           abs(leftPart.top() + leftPart.height() - rightPart.top()) <= gap) )
                     {
                         leftPart.setHeight(leftPart.height() + rightPart.height());
@@ -573,15 +398,15 @@ void DisplayEnumerator::joinWideDisplays()
                         break;
                     }
 
-                    // conter-clock-wise rotation
-                    if (!isHorisontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
+                    // counter-clockwise rotation
+                    if (!isHorizontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
                                           abs(rightPart.top() + rightPart.height() - leftPart.top()) <= gap) )
                     {
                         leftPart.setTop(rightPart.top());
                         leftPart.setHeight(leftPart.height() + rightPart.height());
                         leftPart.setResolutionHeight(leftPart.resolutionHeight() + rightPart.resolutionHeight());
                         removed[j] = true;
-                        qDebug() << "join displays conter-clockwise rotation" << i << j;
+                        qDebug() << "join displays counter-clockwise rotation" << i << j;
                         break;
                     }
 
@@ -625,26 +450,26 @@ void DisplayEnumerator::joinEizoDisplays()
                 leftPart.height() != rightPart.height())
                 continue;
 
-            bool isHorisontal = leftPart.width() < leftPart.height();
+            bool isHorizontal = leftPart.width() < leftPart.height();
             const int gap = 20;
 
-//            isHorisontal = !isHorisontal; // DELL testing
+//            isHorizontal = !isHorizontal; // DELL testing
 
-            qDebug() << "EIZO right display part" << j << "is horizontal" << isHorisontal;
+            qDebug() << "EIZO right display part" << j << "is horizontal" << isHorizontal;
 
-            if (isHorisontal && (abs(leftPart.top() - rightPart.top()) <= gap &&
+            if (isHorizontal && (abs(leftPart.top() - rightPart.top()) <= gap &&
                                  abs(leftPart.left() + leftPart.width() - rightPart.left()) <= gap) )
             {
                 leftPart.setWidth(leftPart.width() + rightPart.width());
                 leftPart.setResolutionWidth(leftPart.resolutionWidth() + rightPart.resolutionWidth());
                 leftPart.setWidth_mm(leftPart.width_mm() + rightPart.width_mm());
                 removed[j] = true;
-                qDebug() << "join EIZO displays horisontally" << i << j;
+                qDebug() << "join EIZO displays horizontally" << i << j;
                 break;
             }
 
             // clock-wise rotation
-            if (!isHorisontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
+            if (!isHorizontal && (abs(leftPart.left() - rightPart.left()) <= gap ||
                                   abs(leftPart.top() + leftPart.height() - rightPart.top()) <= gap) )
             {
                 leftPart.setHeight(leftPart.height() + rightPart.height());
